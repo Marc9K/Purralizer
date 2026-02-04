6f3ce10f9f973efabb88282fa9e27dbc5d9edbc4
Marc <marc@evelize.com>
2026-02-04 17:33:37 +0000

    fix ghp

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </ChakraProvider>
